2261c5a9586658692031dcadf1c173c300ff1ade
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-25 11:42:03 -0500

    Create new.py

---
+++ new.py
@@ -0,0 +1,15 @@
+#Will C.
+#Python 1
+#Unit 1, mod 2
+
+#Task 1
+# [ ] enter a string in the print() function using single quotes
+print('The quick brown fox jumped over the lazy dog.')
+# [ ] enter a string in the print() function using double quotes
+print("I'm actually writing this in VSCode, I hope that's acceptable.")
+
+#Task 2
+# [ ] print an Integer
+print(8)
+# [ ] print a string made of Integer characters
+print("12")
