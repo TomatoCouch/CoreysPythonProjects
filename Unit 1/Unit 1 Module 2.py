b61546d022b83a46813f1ba5ba0dc5b410c4d6a7
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-25 11:55:35 -0500

    cspell (derogatory)

--- Unit 1/Unit 1 Module 2.py
+++ Unit 1/Unit 1 Module 2.py
@@ -13,3 +13,25 @@ print("I'm actually writing this in VSCode, I hope that's acceptable.")
 print(8)
 # [ ] print a string made of Integer characters
 print("12")
+
+#Task 3
+# [ ] Review code and Run
+
+# initialize the variable
+current_msg = "I am a string"
+# print literal string
+print("I am a string")
+# print variable string
+print(current_msg)
+# [ ] Review code and Run
+# assign a new string to the current_msg
+current_msg = "Run this cell using Ctrl+Enter"
+print(current_msg)
+#In the cell below:
+#Print the variable current_msg.
+#Assign a new string to current_msg.
+#Print the variable current_msg again.
+#Run the code cell above and then run the cell below.
+print(current_msg)
+current_msg="aghghahgghahgh"
+print (current_msg)
