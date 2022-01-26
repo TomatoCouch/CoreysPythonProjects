e176f0db24575b61041e6b6e49cd6879e8ea32bf
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-26 11:33:40 -0500

    today's assignment

---
+++ Unit 1/Unit 1 Module 1.3.py
@@ -0,0 +1,26 @@
+#Will C.
+#Python 1
+#Unit 1, mod 1.3
+
+# [ ] show the type after assigning bucket = a whole number value such as 16  
+bucket=7
+print(type(bucket))
+# [ ] show  the type after assigning bucket = a word in "double quotes" 
+bucket="This, is a bucket."
+print(type(bucket))
+# [ ] display the type of 'single quoting' (use single quotes)
+print(type('Dear God.'))
+# [ ] display the type of "double quoting" (use double quotes) 
+print(type("There's more."))
+# [ ] display the type of "12" (use quotes) 
+print(type("12"))
+# [ ] display the type of 12 (no quotes)
+print(type(12)) 
+# [ ] display the type of -12 (no quotes) 
+print(type(-12))
+# [ ] display the type of 12.0 (no quotes) 
+print(type(12.0))
+# [ ] display the type of 1.55
+print(type(1.55)) 
+# [ ] find the type of the type(3) statement (no quotes) - just for fun 
+print(type(type(3)))
