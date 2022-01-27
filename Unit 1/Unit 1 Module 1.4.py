39e12cacda30c289b57e22dac466ef10e00a7175
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-27 11:20:26 -0500

    more code + c(rap)spell

--- Unit 1/Unit 1 Module 1.4.py
+++ Unit 1/Unit 1 Module 1.4.py
@@ -2,3 +2,36 @@
 #Python 1
 #Unit 1, mod 1.4
 
+# [ ] Review and run code for adding a pair of 2 digit Integers
+23 + 18
+# [ ] Review and run code for adding 2 strings 
+"my name is " + "Alyssa"   
+# [ ] Review and run code for adding a variable string and a literal string 
+shoe_color = "brown" 
+"my shoe color is " + shoe_color 
+# [ ] add 3 integer numbers 
+130 + 151 + 864
+# [ ] add a float number and an integer number
+12.1 + 6
+# [ ] Add the string "This notebook belongs to " and a string with your first name 
+"This notebook belongs to " + "Will"
+sm_number=47
+big_number=81726478126871235758
+sm_number + big_number
+first_name="Will"
+# [ ] assign a string value to the variable first_name and add to the string ", remember to save the (I don't know what this says)
+# [ ] review & run code for assigning variables & using addition 
+add_two = 34 + 16 
+first_name = "Alton" 
+greeting = "Happy Birthday " + first_name 
+print(add_two) 
+print(greeting) 
+#  [ ] review & run code for Integer addition in variables and in a print function 
+int_sum = 6 + 7 
+print(int_sum) 
+print(11 + 15) 
+print() 
+# string addition in variables and in print()function 
+hat_msg = "I do not wear " + "a hat"   
+print(hat_msg) 
+print("at " + "dinner") 
