1be04aa7f7f84b738ff88653ab6bd093201be9dc
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-27 12:04:45 -0500

    can't say anything offensive here because my teacher may end up reading this and he probably knows how to check commits

--- Unit 1/Unit 1 Module 1.4.py
+++ Unit 1/Unit 1 Module 1.4.py
@@ -2,13 +2,7 @@
 #Python 1
 #Unit 1, mod 1.4
 
-# [ ] Review and run code for adding a pair of 2 digit Integers
-23 + 18
-# [ ] Review and run code for adding 2 strings 
-"my name is " + "Alyssa"   
-# [ ] Review and run code for adding a variable string and a literal string 
-shoe_color = "brown" 
-"my shoe color is " + shoe_color 
+#Task 1
 # [ ] add 3 integer numbers 
 130 + 151 + 864
 # [ ] add a float number and an integer number
@@ -20,18 +14,41 @@ big_number=81726478126871235758
 sm_number + big_number
 first_name="Will"
 # [ ] assign a string value to the variable first_name and add to the string ", remember to save the (I don't know what this says)
-# [ ] review & run code for assigning variables & using addition 
-add_two = 34 + 16 
-first_name = "Alton" 
-greeting = "Happy Birthday " + first_name 
-print(add_two) 
-print(greeting) 
-#  [ ] review & run code for Integer addition in variables and in a print function 
-int_sum = 6 + 7 
-print(int_sum) 
-print(11 + 15) 
-print() 
-# string addition in variables and in print()function 
-hat_msg = "I do not wear " + "a hat"   
-print(hat_msg) 
-print("at " + "dinner") 
+
+#Task 2
+# [ ] perform string addition in the variable named new_msg (add a string to "my favorite food is ")
+new_msg = "My favorite food is " + "pizza"
+print(new_msg) 
+# [ ] perform Integer addition in the variable named new_sum (add 2 or more Integers) 
+new_sum =   0 + 1 + 2 + 3
+print(new_sum) 
+# [ ] create and print a new string variable, new_msg_2, that concatenates new_msg + a literal string
+new_msg_2=new_msg + " a literal string"
+print(new_msg_2)
+
+#Task 3
+# [ ] Review and run the code - then fix any Errors 
+total_cost = 3 + 45
+print(total_cost) 
+# [ ] Review and run the code - then fix any Errors 
+school_num = "123" 
+print("the street number of Central School is " + school_num) 
+# [ ] Read and run the code - write a hypothesis for what you observe adding float + int 
+#  [ ] HYPOTHESIS:  
+print(type(3.3)) 
+print(type(3)) 
+print(3.3 + 3) 
+
+#Task 4
+# [ ] Repair the syntax error  
+print("my socks do not match")  
+# [ ] Repair the NameError   
+print("my socks match now")  
+# [ ] Repair the syntax error  
+print("Save the notebook frequently") 
+# [ ] Repair the NameError  
+student_name = "Alton" 
+print(student_name) 
+# [ ] Repair the TypeError 
+total = "3" 
+print(total + " students are signed up for tutoring") 
