8ed6e8875b2db9ca4e120def444821567f1493f8
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-28 11:34:18 -0500

    pipe bomb

---
+++ the lab.py
@@ -0,0 +1,23 @@
+#The Laboratory
+#Where I can tinker around
+
+#print("Do you see this?") (to the comment realm with you, bastard)
+#if input("Yes"):print("Great") (didn't work)
+#if askstring(prompt="can you see this?"):print ("Great!") (no clue what i'm doing)
+
+#how do boolean
+#bool=True (commented to not print before my pipe bomb script)
+#print(type(True)) (ditto)
+#ok then
+#are_these_case_sensitive=True (commented to not print before my pipe bomb script)
+#print(are_these_case_sensitive) (ditto)
+
+#now for The Funny(tm)
+is_there_a_pipe_bomb_in_your_mailbox=True
+print("Let's see if there's a pipe bomb in your mailbox.")
+print("In order to do so, we'll check the boolean is_there_a_pipe_bomb_in_your_mailbox.")
+print("The boolean is:")
+print(is_there_a_pipe_bomb_in_your_mailbox)
+if(is_there_a_pipe_bomb_in_your_mailbox==True): 
+    print("Run."), 
+else: print("You're safe, for now.")
