76516048978c56d1a91f167bafb1500602e435ff
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-01-28 11:07:04 -0500

    bird + e

---
+++ Unit 1/Unit 1 Module 1.5.py
@@ -0,0 +1,18 @@
+#Will C.
+#Python 1
+#Unit 1, mod 1.5
+
+# [ ] flying bird character art
+print("-        -")
+print(" \\     /")
+print("  \\. ./")
+print("    V")
+print(" haha look at the bird")
+# [ ] capital letter "E" character art
+print("__________")
+print("|")
+print("|")
+print("|------")
+print("|")
+print("|_________")
+print("e")
