ed1258f582d8a7ddb37ae43fd78b46121c3f66e1
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-01 11:09:03 -0500

    1.7

---
+++ Unit 1/Unit 1 Module 1.7.py
@@ -0,0 +1,21 @@
+#Will C.
+#Python 1
+#Unit 1, mod 1.7
+
+#Task 1
+# [ ] print 3 strings on the same line using commas inside the print() function
+print("There is","a comma","in this print statement.")
+
+#Task 2
+# [ ] use a print() function with comma separation to combine 2 numbers and 2 strings 
+print(1,"comma",2,"commas")
+
+#Task 3
+# [ ] get user input for a street name in the variable, street
+print("What is the street name?")
+street=input()
+# [ ] get user input for a street number in the variable, st_number
+print("What is the street number?")
+st_number=input()
+# [ ] display a message about the street and st_number
+print("The address is",st_number,street)
