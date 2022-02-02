965d9b64b487648d80558af1802fe5ffa62d28a6
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-02 12:07:43 -0500

    homework and 1.8

---
+++ Unit 1/Unit 1 Module 1 Lab 1b.py
@@ -0,0 +1,53 @@
+#Will C.
+#Python 1
+#Homework assignment
+
+# [ ] get user input for a variable named remind_me
+print("Add a reminder:")
+remind_me=input()
+# [ ] print the value of the variable remind_me 
+print(remind_me)
+# use string addition to print "remember: " before the remind_me input string
+print("Remember:",remind_me)
+
+#Task 2
+# [ ] get user input for 2 variables: meeting_subject and meeting_time
+print("What will your meeting be about?")
+meeting_subject=input()
+print("And what time will your meeting be?")
+meeting_time=input() + "."
+# [ ] use string addition to print meeting subject and time with labels
+print("Your meeting about",meeting_subject,"will start at",meeting_time)
+
+#Task 3
+# [ ] print the combined strings "Wednesday is" and "in the middle of the week"
+print("Wednesday is","in the middle of the week")
+# [ ] print combined string "Remember to" and the string variable remind_me from input above
+print("Remember to",remind_me)
+# [ ] Combine 3 variables from above with multiple strings
+print("Remember to",remind_me,"and don't forget your meeting about",meeting_subject,"at",meeting_time)
+
+#Task 4? These aren't seperated with task numbers so I'm just guessing
+# [ ] print a string sentence that will display an Apostrophe (')
+print("'")
+# [ ] print a string sentence that will display an Apostrophe (')
+print("'")
+#that was easy
+#that's what it showed up as on the assignment llllllllllllllllllllllllL
+
+#Task 5
+# [ ] complete vehicle tests
+vehicle=input("Name a vehicle:")
+# [ ] print True or False if color starts with "b"
+print("color".startswith("b"))
+
+#Task 6
+# [ ] get user input for a variable named color
+color=input("Choose a color:")
+# [ ] modify color to be all lowercase and print
+
+# [ ] get user input using variable remind_me and format to all **lowercase** and print
+
+# [ ] test using input with mixed upper and lower cases
+
+# [] get user input for the variable yell_this and format as a "YELL" to ALL CAPS
---
+++ Unit 1/Unit 1 Module 1.8.py
@@ -0,0 +1,22 @@
+#Will C.
+#Python 1
+#Unit 1, mod 1.8
+
+#Task 1
+# [ ] using a print statement, display the text: Where's the homework? 
+
+
+print("Where's the homework?")
+# [ ] output with double quotes: "Education is what remains after one has forgotten what one has learned in school."
+print('Education is what remains after one has forgotten what one has learned in school.')
+
+#Task 2
+# [ ] Use .isalpha() on the string "alphabetical" 
+print("alphabetical".isalpha())
+# [ ] Use .isalpha() on the string: "Are spaces and punctuation Alphabetical?"
+print("Are spaces and punctuation Alphabetical?".isalpha())
+# [ ] initialize variable alpha_test with input
+alpha_test="test string"
+# [ ] use .isalpha() on string variable alpha_test
+print(alpha_test.isalpha())
+
