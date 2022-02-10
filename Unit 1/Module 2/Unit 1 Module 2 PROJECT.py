bc80c039b9f394952d8af91cf7a92d2147c9cc69
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-10 11:28:06 -0500

    cspill your own blood

---
+++ Unit 1/Module 2/Unit 1 Module 2 PROJECT.py
@@ -0,0 +1,18 @@
+#Will C. & Luke H.
+#Python 1
+#Unit 1 mod 2 project
+# [ ] create, call and test fishstore() function
+def fishstore(fish, quantity):
+    sentence = "Fish Type: " + fish + "Quantity: " + quantity
+    return sentence
+def stock(in_stock):
+     in_stock = "salmon, trout, grouper, bass, catfish, flounder"
+     return (fish_entry.lower() in in_stock)
+fish_entry = input("Enter a fish name: ")
+quantity = input("How many would you like? ")
+fish_stock = stock
+if stock==True:
+    str_stock = "This fish is in stock"
+else:
+    str_stock = "This fish is not in stock"
+print(fishstore(fish_entry,quantity), str_stock)
