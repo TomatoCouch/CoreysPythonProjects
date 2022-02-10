ce529d42e8f4806bc24e64786ba26bdd01cf01d3
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-10 11:41:49 -0500

    pushing from github again

--- Unit 1/Module 2/Unit 1 Module 2 PROJECT.py
+++ Unit 1/Module 2/Unit 1 Module 2 PROJECT.py
@@ -3,12 +3,12 @@
 #Unit 1 mod 2 project
 # [ ] create, call and test fishstore() function
 def fishstore(fish, quantity):
-    sentence = "Fish Type: " + fish + "Quantity: " + quantity
+    sentence = "Fish Type: " + fish + " Quantity: " + quantity
     return sentence
 in_stock = "salmon, trout, grouper, bass, catfish, flounder"
 fish_entry = input("Enter a fish name: ")
 quantity = input("How many would you like? ")
-fish_stock = (fish_entry.lower() in in_stock)
+fish_stock = (fish_entry.lower() in (in_stock))
 stock = fish_stock
 if stock==True:
     str_stock = "This fish is in stock"
