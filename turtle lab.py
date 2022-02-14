f6dd117b72e58c7cd59df83173441f9ed03ccc47
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-14 11:39:02 -0500

    turtle

--- turtle lab.py
+++ turtle lab.py
@@ -8,12 +8,30 @@
 #what
 #maybe if i didn't make the file name and library name as the same thing, this would've worked first time???
 #god i'm stupid
+#import turtle
+
+#s = turtle.getscreen()
+#t = turtle.Turtle()
+
+#t.right(90)
+#t.forward(100)
+#t.left(45)
+#t.backward(100)
+#t.left(45)
+#t.forward(100)
 import turtle
 
-s = turtle.getscreen()
-t = turtle.Turtle()
+s=turtle.getscreen
+t=turtle.Turtle()
 
 t.right(90)
 t.forward(100)
-t.left(90)
-t.backward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(135)
+t.forward(145)
+t.right(45)
