ebf46241cc4e38c6b261b6e9a6e298733e543a5c
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-02-24 11:58:25 -0500

    isn't around

--- Unit 1/Module 4/Unit 1 Module 4.1.py
+++ Unit 1/Module 4/Unit 1 Module 4.1.py
@@ -28,40 +28,56 @@ if do_start.lower()=="y":
 elif do_start.lower()=="yes":
     print("Great! ")
     start=True
+    startBad=False
 else:
     print("Too bad.")
     startBad=True
+    start=False
 birds="cardinal bluejay tit"
 if start==True:
     print("Rules:")
     print("")
     print("3 birds have been named. Guess one of the 3 to win!")
     doQuestions=True
+    doQuestionsBad=False
 elif startBad==True:
     print("Rules:")
     print("")
     print("Since you don't want to do this, you get one shot. Name one of the 3 birds I've selected, then get out of here.")
     doQuestionsBad=True
+    doQuestions=False
 else:
     print("how")
 if doQuestions==True:
     guess1=input("First guess: ")
-    if (guess1.lower())in(birds)==True:
+    if (guess1.lower()in(birds))==True:
         print("Correct bird! 1/3")
         quit
     else: 
         print("Incorrect bird.")
     guess2=input("Second guess: ")
-    if guess2 in(birds)==True:
+    if (guess2.lower()in(birds))==True:
         print("Correct bird! 2/3")
         quit
     else:
         print("Incorrect bird.")
     guess3=input("Final guess: ")
-    if guess3 in(birds)==True:
+    if (guess3.lower()in(birds))==True:
         print("Correct bird! 3/3")
         quit
     else:
         print("Incorrect bird.")
         print("Game over!")
         quit
+else:
+    dummyValue=False
+if doQuestionsBad==True:
+    guess1bad=input("Guess. ")
+    if (guess1bad.lower()in(birds))==True:
+        print("Wow, you did it. Now get out.")
+        quit
+    else:
+        print("Wow, you suck. Now get out.")
+        quit
+else:
+    print("You shouldn't be this far down")
