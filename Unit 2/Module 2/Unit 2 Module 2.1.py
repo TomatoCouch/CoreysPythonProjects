c1077e0a26542990f51383b5a0d3a3713ded8ca4
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-03-09 12:04:39 -0500

    2.1

---
+++ Unit 2/Module 2/Unit 2 Module 2.1.py
@@ -0,0 +1,30 @@
+#Will C
+#Python 1
+#Unit 2, Mod 2.1
+
+#Task 1
+# [ ] create team_names list and populate with 3-5 team name strings
+team_names=["One","Two","Three","Four","Five"]
+# [ ] print the list 
+print(team_names)
+# [ ] Create a list mix_list with numbers and strings with 4-6 items
+mix_list=[8,"asdh","lksadnuo",6,6,"sdfjb"]
+# [ ] print the list
+print(mix_list)
+
+#Task 2
+# [ ] Create a list, streets, that lists the name of 5 street name strings
+streets=["Main","Walnut","1st","Gant","Dane"]
+# [ ] print a message that there is "No Parking" on index 0 or index 4 streets
+print("There is no parking on",streets[0],"or",streets[4],"street.")
+# [ ] Create a list, num_2_add, made of 5 different numbers between 0 - 25
+num_2_add=[4,8,17,24,6]
+# [ ] print the sum of the numbers
+print(num_2_add[0]+num_2_add[1]+num_2_add[2]+num_2_add[3]+num_2_add[4])
+
+#Task 3
+# [ ] Review & Run, but ***Do Not Edit*** this code cell
+#no
+# [ ] Fix the error by only editing and running the block below
+pay_checks=[1000,812,584,67]
+print(" Total of checks 3 & 4 = $", pay_checks[2] + pay_checks[3])
