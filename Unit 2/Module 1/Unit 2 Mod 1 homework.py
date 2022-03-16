419a1e58414dfcfae484567a6fc57bd3353abdda
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-03-16 11:33:36 -0400

    homework 2

--- Unit 2/Module 1/Unit 2 Mod 1 homework.py
+++ Unit 2/Module 1/Unit 2 Mod 1 homework.py
@@ -64,6 +64,30 @@ name = "Hiroto"
 print(name[2::2])
 
 #Task 4
+#Get user input for first_name
+#Create an empty string variable: new_name
+#Iterate through letters in first_name Backwards
+#Add each letter to new_name as you iterate
+#Replace the letter if "e", "t" or "a" with "?" (hint: if, elif, elif, else)
+#Print new_name
+# [ ] Create Mystery Name 
+first_name=input("What is your first name? ")
+new_name=""
+counter=0
+while counter!=len(first_name):
+    print("loop works")
+    if first_name.lower()[-(counter)]=='e':
+        new_name+="?"
+    elif first_name.lower()[-(counter)]=='t':
+        new_name+="?"
+    elif first_name.lower()[-(counter)]=='a':
+        new_name+="?"
+    else:
+        new_name+=first_name[-(counter)]
+    counter+=1
+print(new_name)
+
+#Task 5
 # [ ] find and display the length of the string: topic 
 topic = ".find() returns the length of a string"
 print(len(topic))
@@ -79,3 +103,30 @@ print(work_tip.find("code"))
 # [ ] save result in varible: code_index 
 # [ ] display index of where "code" is found, or print "not found" if code_index == -1 
 work_tip = "Good code is commented code" 
+
+#Task 6
+# [ ] find and report (print) number of w's, o's + use of word "code" 
+work_tip = "Good code is commented code"
+print("Number of 'w's:\t\t",work_tip.find("w"))
+print("Number of 'o's:\t\t",work_tip.find("o"))
+print("Number of 'code's:\t",work_tip.find("code"))
+# [ ]  count times letter "i" appears in code_tip string 
+# [ ] find and display the index of all the letter i's in code_tip 
+# Remember: if .find("i") has No Match, -1 is returned 
+code_tip = "code a conditional decision like you would say it" 
+print ("code_tip:" , code_tip)
+print(code_tip.count('i'))
+print(code_tip.find('i'))
+
+#Task 7
+#Create a program that inputs a quote (a sentence without punctuation) and prints all of the words that start with
+#h-z.
+#Split the words by building a placeholder variable: word, and adding a letter each loop until a non-alpha char
+#is encountered
+#if non-alpha detected (space, punctuation, digit,...) defines the end of a word
+#else, check if word is greater than "g" alphabetically, print word and set word = empty string
+#or else set word = empty string and build the next word
+#Hint: use  .lower() .
+# [] create words after "G" 
+# sample quote "Wheresoever you go, go with all your heart" ~ Confucius (551 BC - 479 BC)
+quote='"Wheresoever you go, go with all your heart" ~ Confucius (551 BC - 479 BC)'
