5fb4ce1b97e23e71aa18597fe500f70bc95c9021
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-03-29 16:13:03 -0400

    god

--- Unit 2/Module 4/Unit 2 Module 4.1.py
+++ Unit 2/Module 4/Unit 2 Module 4.1.py
@@ -1,6 +1,19 @@
 #Will C.
 #Python 1
 #Unit 2, Mod 4.1
+#This whole thing is broken, I'm taking a look at it eventually
 
+#Task 1
+cities_file=open('cities.txt','r')
+print(cities_file)
 
-cities=open('cities.txt','r')
+#Task 2
+# [ ] after import and open of cities.txt in task 1 
+# [ ] read cities_file as cities 
+cities=cities_file.read()
+# [ ] display the string: cities 
+cities
+# [ ] print the string: cities 
+print(cities)
+
+#Task 3
