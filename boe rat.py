e78a003a400ce9ceafe4c0d76cbe2384d96aa657
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-04-01 20:01:55 -0400

    borat

---
+++ boe rat.py
@@ -0,0 +1,7 @@
+while True:
+    answer=input("Would you like to install Borat? [Y or N]\n")
+    if answer.lower()=="y":
+        print("Good.")
+        break
+    elif answer.lower()=="n":
+        print("That is not an option.")
