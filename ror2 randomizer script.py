e1d2dce8743851d8fb0e5aad031f9b6370cb792e
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-04-15 15:12:06 -0400

    final ror2 script iteration

--- ror2 randomizer script.py
+++ ror2 randomizer script.py
@@ -1,15 +1,21 @@
 from random import shuffle
 from random import randint
 survivorList=["Commando","Huntress","Bandit","Mul-T","Engineer","Artificer","Mercenary","REX","Loader","Acrid","Captain","Railgunner","「V??oid Fiend』"]
-shuffle(survivorList)
-playerList=[]
 while True:
-    playerName=input("Enter a player name. To quit, enter nothing. ")
-    if playerName=="":
-        break
+    shuffle(survivorList)
+    playerList=[]
+    while True:
+        playerName=input("Enter a player name. To quit, enter nothing. ")
+        if playerName=="":
+            break
+        else:
+            playerList.append(playerName)
+            print("Players:\n",playerList)
+    for name in playerList:
+        randomInteger=randint(0,13)
+        print(name,":\n",survivorList[randomInteger])
+    keep=input("Is this satisfactory? ")
+    if keep.lower()=="yes":
+            break
     else:
-        playerList.append(playerName)
-        print("Players:\n",playerList)
-for name in playerList:
-    randomInteger=randint(0,13)
-    print(name,":\n",survivorList[randomInteger])
+            pass
