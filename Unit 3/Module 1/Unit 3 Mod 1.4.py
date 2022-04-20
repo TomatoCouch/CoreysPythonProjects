6b56afe3f2999967391a9b8fae7dfaaea7a9065e
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-04-20 11:38:09 -0400

    doesn't work lmao

--- Unit 3/Module 1/Unit 3 Mod 1.4.py
+++ Unit 3/Module 1/Unit 3 Mod 1.4.py
@@ -13,19 +13,29 @@ dir_name=input("Enter a directory name: ")
 print("Making",dir_name)
 os.mkdir(dir_name)
 # 3) Verify the directory was created by listing the content of the current working directory
-print(os.chdir())
+print(os.listdir())
 # 4) Remove the created directory
 os.rmdir(dir_name)
 # 5) Verify the directory was removed by listing the content of the current working directory 
-print(os.chdir())
+print(os.listdir())
 
 #Task 2
 # [ ] Write a program that prompts the user for a file or directory name
 name=input("Enter a file/directory name: ")
 # then prints a message verifying if it exists in the current working directory
-
-# [ ] Write a program to print the absolute path of all directories in "parent_dir" 
+if os.path.isdir(name)==True:
+    print("Real directory!")
+else:
+    print("Fake directory.")
+# [ ] Write a program to print the absolute path of all directories in "parent_dir"
 # HINTS: 
 # 1) Verify you are inside "parent_dir" using os.getcwd() 
+if os.getcwd=="parent_dir":
+
 # 2) Use os.listdir() to get a list of files and directories in "parent_dir" 
+   dadDir=os.listdir()
+else:
+    print("Not in parent_dir")
 # 3) Iterate over the elements of the list and print the absolute paths of all the directories 
+for dir in dadDir:
+    print(os.path.abspath(dir))
