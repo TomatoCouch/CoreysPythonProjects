a3e6b7883f7829fa630803aaf55b54225e6f40be
TomatoCouch <harrypotterisawesome78@gmail.com>
2022-04-27 14:12:49 -0400

    added message

--- r to d.py
+++ r to d.py
@@ -7,20 +7,24 @@ doRad=False
 doDeg=True
 while True:
     if doDeg==True and doRad==False:
-        num=input("Enter your degrees. To quit, enter quit. To switch conversion, enter swap.\n")
+        num=input("Enter your degrees. To quit, enter quit. To switch conversion, enter swap. If you'd like pi to remain in your answers, enter pi.\nNOTE: Entering anything that isn't a number will cause an error. Don't do that.\n")
         if num.lower()=="quit":
             quit()
         elif num.lower()=="swap":
             doDeg=False
             doRad=True
+        elif num.lower()=="pi":
+            print("no")
         else:
             print(num,"degrees is equivalent to:",float(num)*(pi/180.0),"radians.")
     elif doDeg==False and doRad==True:
-        num=input("Enter your radians. To quit, enter quit. To switch conversion, enter swap.\n")
+        num=input("Enter your radians. To quit, enter quit. To switch conversion, enter swap. If you'd like pi to remain in your answers, enter pi.\nNOTE: Entering anything that isn't a number will cause an error. Don't do that.\n")
         if num.lower()=="quit":
             quit()
         elif num.lower()=="swap":
             doDeg=True
             doRad=False
+        elif num.lower()=="pi":
+            print("no")
         else:
             print(num,"radians is equivalent to:",float(num)*(180.0/pi),"radians.")
